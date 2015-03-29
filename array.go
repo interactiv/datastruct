9072f586974c8e73d52ef768b84a5d1a4c72f457
mparaiso <aikah@free.fr>
2015-03-29 08:17:20 +0200

    first commit

---
+++ array.go
@@ -0,0 +1,249 @@
+// Copyright 2015 mparaiso<mparaiso@online.fr>. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// datastruct provides common datastructures for programming such as Arrays , LinkedLists , Maps , Stack and Queues
+// This package use functional programming concepts extensively, making working with arrays every much more
+// easy than arrays and slices provided by Go.
+
+package datastruct
+
+import (
+	"sort"
+)
+
+// Array is a alternative structure for the default array implementation
+type Array struct {
+	array []interface{}
+}
+
+// ArrayInterface represents all methods of an array
+type ArrayInterface interface {
+	Push(values ...interface{}) int
+	Pop() interface{}
+	At(int) interface{}
+	Length() int
+	Shift() interface{}
+	Unshift(values ...interface{}) int
+	ForEach(func(interface{}, int))
+	Reduce(func(interface{}, interface{}, int) interface{}, interface{}) interface{}
+	Map(func(interface{}, int) interface{}) ArrayInterface
+	Slice(v ...int) ArrayInterface
+	Splice(start int, deleteCount int, items ...interface{}) ArrayInterface
+	Some(func(interface{}, int) bool) bool
+	Every(func(interface{}, int) bool) bool
+	Reverse() ArrayInterface
+	Concat(arrays ...ArrayInterface) ArrayInterface
+	Sort(func(a, b interface{}) bool) ArrayInterface
+}
+
+// NewArray returns a new array
+func NewArray(values ...interface{}) *Array {
+	array := &Array{}
+	for i := 0; i < len(values); i++ {
+		array.Push(values[i])
+	}
+	return array
+}
+
+// At get a value at index
+func (a *Array) At(index int) interface{} {
+	var result interface{}
+	if index < len(a.array) && index >= 0 {
+		result = a.array[index]
+	}
+	return result
+}
+
+// Push put values at the end of array
+// returns the number of values added
+func (a *Array) Push(values ...interface{}) int {
+	a.array = append(a.array, values...)
+	return len(values)
+}
+
+// Pop remove the last value of the array
+func (a *Array) Pop() interface{} {
+	var result interface{}
+	if len(a.array) > 0 {
+		result = a.array[len(a.array)-1]
+		a.array = a.array[:len(a.array)-1]
+	}
+	return result
+}
+
+// Length returns the number of elements of the array
+func (a *Array) Length() int {
+	return len(a.array)
+}
+
+// Shift removes the first element of the array and returns it
+func (a *Array) Shift() interface{} {
+	var result interface{}
+	if len(a.array) > 0 {
+		result = a.array[0]
+	}
+	a.array = a.array[1:]
+	return result
+}
+
+// Unshift add elements at index 0 and returns the number of added elements
+func (a *Array) Unshift(values ...interface{}) int {
+
+	for _, value := range values {
+		a.array = append(append([]interface{}{}, value), a.array...)
+	}
+	return len(values)
+}
+
+// ForEach execute callback on each element of the array
+func (a *Array) ForEach(callback func(value interface{}, i int)) {
+	for i := 0; i < len(a.array); i++ {
+		callback(a.array[i], i)
+	}
+}
+
+// Reduce folds the array into a single value
+func (a *Array) Reduce(callback func(result interface{}, value interface{}, index int) interface{}, initial interface{}) interface{} {
+	a.ForEach(func(value interface{}, i int) {
+		initial = callback(initial, value, i)
+	})
+	return initial
+
+}
+
+// Map iterate over array and push the result of callback into a new Array
+func (a *Array) Map(callback func(value interface{}, i int) interface{}) ArrayInterface {
+	return a.Reduce(func(result interface{}, value interface{}, index int) interface{} {
+		result.(ArrayInterface).Push(callback(value, index))
+		return result
+	}, NewArray()).(ArrayInterface)
+}
+
+// Sort sorts an array given a compare function
+func (a *Array) Sort(compareFunc func(a, b interface{}) bool) ArrayInterface {
+	result := a.Slice()
+	sort.Sort(&sorter{result, compareFunc})
+	return result
+}
+
+// Splice remove elements from the array at a given index and optionally insert new elements
+func (a *Array) Splice(start int, deleteCount int, items ...interface{}) ArrayInterface {
+	var result ArrayInterface
+	head := a.Slice(0, start)
+	queue := a.Slice(start + deleteCount)
+	result = a.Slice(start, start+deleteCount)
+	a.array = []interface{}{}
+	for i := 0; i < head.Length(); i++ {
+		a.Push(head.At(i))
+	}
+	a.Push(items...)
+	for i := 0; i < queue.Length(); i++ {
+		a.Push(queue.At(i))
+	}
+	return result
+}
+
+// Slice returns a copy of a portion of the array
+// It takes up to 2 arguments :
+// 	- begin int
+// 	- end int (excluded)
+//
+func (a *Array) Slice(beginAndEndValues ...int) ArrayInterface {
+	var begin, end int
+	values := beginAndEndValues
+	if len(values) > 0 {
+		begin = values[0]
+		if len(values) > 1 {
+			end = values[1]
+		} else {
+			end = a.Length()
+		}
+	} else {
+		return NewArray(a.array...)
+	}
+	if begin < 0 {
+		if a.Length()+begin < 0 {
+			begin = 0
+		} else {
+			begin = a.Length() + begin
+		}
+	}
+	if end < 0 {
+		if a.Length()-end < 0 {
+			return NewArray()
+		}
+		end = a.Length() + end
+
+	}
+	if end <= begin {
+		return NewArray()
+	}
+
+	return NewArray(a.array[begin:end]...)
+}
+
+// Some returns true if the callback predicate is satisfied
+func (a *Array) Some(callback func(v interface{}, index int) bool) bool {
+	for i, v := range a.array {
+		if callback(v, i) {
+			return true
+		}
+	}
+	return false
+}
+
+// Every returns true if the callback predicate is true for every element of the array
+func (a *Array) Every(callback func(v interface{}, index int) bool) bool {
+	for i, value := range a.array {
+		if !callback(value, i) {
+			return false
+		}
+	}
+	return true
+}
+
+// Reverse reverse the order of the elements of the array and returns a new one
+func (a *Array) Reverse() ArrayInterface {
+	var result ArrayInterface = NewArray()
+	for i := a.Length() - 1; i >= 0; i-- {
+		result.Push(a.array[i])
+	}
+	return result
+}
+
+// Concat adds arrays to the end of the array and returns an new array
+func (a *Array) Concat(arrays ...ArrayInterface) ArrayInterface {
+	result := NewArray(a.array...)
+	for _, array := range arrays {
+		array.ForEach(func(val interface{}, i int) {
+			result.Push(val)
+		})
+	}
+	return result
+}
+
+// sorter is used for array.Sort
+type sorter struct {
+	array       ArrayInterface
+	compareFunc func(a, b interface{}) bool
+}
+
+// Len returns the length of the array
+func (s *sorter) Len() int {
+	return s.array.Length()
+}
+
+// Less compare 2 elements. if i is less than j return true ,else return false
+func (s *sorter) Less(i, j int) bool {
+	if s.compareFunc(s.array.At(i), s.array.At(j)) == true {
+		return true
+	}
+	return false
+}
+
+// Swap swaps 2 elements
+func (s *sorter) Swap(i, j int) {
+	a := s.array.Splice(i, 1, s.array.At(j))
+	s.array.Splice(j, 1, a.At(0))
+}
---
+++ array_test.go
@@ -0,0 +1,299 @@
+// Copyright 2015 mparaiso<mparaiso@online.fr>. All rights reserve
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package datastruct
+
+import (
+	"testing"
+)
+
+func expect(t *testing.T, actual interface{}, expected interface{}) {
+
+	if actual != expected {
+		t.Error(actual, "should be", expected)
+	}
+}
+
+func TestLength(t *testing.T) {
+	a := NewArray(1, 2, 3)
+	expected := 3
+	actual := a.Length()
+	expect(t, expected, actual)
+}
+
+// TestNewArray tests array constructor
+func TestNewArray(t *testing.T) {
+	a := NewArray(1, 2, 3)
+	expect(t, a.At(0), 1)
+	// test if pointers are properly supported
+	b := NewArray(&struct{ x int }{x: 1})
+	expect(t, b.At(0).(*struct{ x int }).x, 1)
+}
+
+func TestPush(t *testing.T) {
+	a := NewArray()
+	a.Push("foo", "bar")
+	NewArray("foo", "bar").ForEach(func(val interface{}, i int) {
+		expect(t, a.At(i), val)
+	})
+
+}
+
+func TestPop(t *testing.T) {
+	popped := "bar"
+	a := NewArray("foo", popped)
+	b := a.Pop()
+	if b != "bar" {
+		t.Error(b, "should be", popped)
+	}
+}
+
+func TestForEach(t *testing.T) {
+	result := 0
+	expected := 10
+	a := NewArray(1, 2, 3, 4)
+	a.ForEach(func(v interface{}, i int) {
+		result += v.(int)
+	})
+	if result != 10 {
+		t.Error(result, "should be", expected)
+	}
+}
+
+func TestReduce(t *testing.T) {
+	a := NewArray(1, 2, 3, 4)
+	b := a.Reduce(func(r, v interface{}, i int) interface{} {
+		return r.(int) + v.(int)
+	}, 0)
+	if b != 10 {
+		t.Error(b, "should be 10")
+	}
+}
+
+func TestMap(t *testing.T) {
+	a := NewArray(1, 2, 3)
+	expected := NewArray(2, 4, 6)
+	b := a.Map(func(v interface{}, i int) interface{} {
+		return v.(int) * 2
+	})
+	b.ForEach(func(v interface{}, i int) {
+		if v != expected.At(i) {
+			t.Errorf("%v should be %v", v, expected.At(i))
+		}
+	})
+
+}
+
+func TestAt(t *testing.T) {
+	a := NewArray("foo", "bar", "baz")
+	for i, v := range []string{"foo", "bar", "baz"} {
+		if a.At(i) != v {
+			t.Errorf("%v should be %v", a.At(i), v)
+		}
+	}
+}
+
+func TestUnshift(t *testing.T) {
+	a := NewArray("baz")
+	a.Unshift("foo", "bar")
+	expected := []string{"bar", "foo", "baz"}
+	for i, v := range expected {
+		if a.At(i) != v {
+			t.Errorf("%v should be %v", a.At(i), v)
+		}
+	}
+}
+
+func TestShift(t *testing.T) {
+	a := NewArray("foo", "bar")
+	b := a.Shift()
+	if a.At(0) != "bar" {
+		t.Errorf("a[0] should be bar")
+	}
+	if b != "foo" {
+		t.Error("b should be 'foo'")
+	}
+}
+
+func TestSplice(t *testing.T) {
+	type fixture struct {
+		array     ArrayInterface
+		arguments []interface{}
+		expected  ArrayInterface
+	}
+	fixtures := NewArray(
+		&fixture{
+			NewArray(1, 2, 3),
+			[]interface{}{0, 1, 4, 5},
+			NewArray(4, 5, 2, 3),
+		},
+	)
+	fixtures.ForEach(func(fix interface{}, index int) {
+		args := fix.(*fixture).arguments
+		fix.(*fixture).array.Splice(args[0].(int), args[1].(int), args[2:]...)
+		fix.(*fixture).expected.ForEach(func(val interface{}, i int) {
+			expect(t, fix.(*fixture).array.At(i), val)
+		})
+	})
+}
+
+func TestSlice(t *testing.T) {
+	type fixture struct {
+		array *Array
+		Slice struct {
+			Begin int
+			End   int
+		}
+		Expected []int
+	}
+	array := NewArray(1, 2, 3, 4, 5)
+	fixtures := []*fixture{
+		&fixture{
+			array: array,
+			Slice: struct {
+				Begin int
+				End   int
+			}{Begin: 1, End: 4},
+			Expected: []int{2, 3, 4},
+		},
+		&fixture{
+			array:    array,
+			Slice:    struct{ Begin, End int }{Begin: -3, End: array.Length()},
+			Expected: []int{3, 4, 5},
+		},
+		&fixture{
+			array:    array,
+			Slice:    struct{ Begin, End int }{Begin: 0, End: -2},
+			Expected: []int{1, 2, 3},
+		},
+	}
+	for _, fixture := range fixtures {
+		//t.Logf("%+v", fixture)
+		actual := fixture.array.Slice(fixture.Slice.Begin, fixture.Slice.End)
+		for i, v := range fixture.Expected {
+			expect(t, actual.At(i), v)
+		}
+	}
+
+}
+
+func TestSome(t *testing.T) {
+	type fixture struct {
+		array    ArrayInterface
+		cb       func(interface{}, int) bool
+		expected bool
+	}
+	fixtures := []*fixture{
+		&fixture{
+			array: NewArray(1, 2, 3, 4, 5, -6),
+			cb: func(value interface{}, index int) bool {
+				return value.(int) >= 0
+			},
+			expected: true,
+		},
+		&fixture{
+			array: NewArray(1, 2, 3),
+			cb: func(v interface{}, i int) bool {
+				return v.(int) == 0
+			},
+			expected: false,
+		},
+	}
+	for _, fixture := range fixtures {
+		expect(t, fixture.array.Some(fixture.cb), fixture.expected)
+	}
+
+}
+
+func TestEvery(t *testing.T) {
+	type fixture struct {
+		array    ArrayInterface
+		cb       func(value interface{}, index int) bool
+		expected bool
+	}
+	isOdd := func(v interface{}, i int) bool {
+		return v.(int)%2 == 0
+	}
+	fixtures := NewArray(
+		fixture{
+			NewArray(2, 4, 6),
+			isOdd,
+			true,
+		},
+		fixture{
+			NewArray(0, 2, 4, 5),
+			isOdd,
+			false,
+		},
+	)
+	fixtures.ForEach(func(v interface{}, i int) {
+		fixture := v.(fixture)
+		expect(t, fixture.array.Every(fixture.cb), fixture.expected)
+	})
+}
+
+func TestReverse(t *testing.T) {
+	type fixture struct {
+		array    ArrayInterface
+		expected ArrayInterface
+	}
+	fixtures := NewArray(
+		fixture{
+			NewArray(1, 2, 3),
+			NewArray(3, 2, 1),
+		},
+	)
+	fixtures.ForEach(func(v interface{}, i int) {
+		reversed := v.(fixture).array.Reverse()
+		reversed.ForEach(func(val interface{}, i int) {
+			expect(t, val, v.(fixture).expected.At(i))
+		})
+	})
+}
+
+func TestConcat(t *testing.T) {
+	a := NewArray(1, 2)
+	b := NewArray(3, 4)
+	c := NewArray(5, 6)
+	expected := NewArray(1, 2, 3, 4, 5, 6)
+	actual := a.Concat(b, c)
+	expected.ForEach(func(val interface{}, i int) {
+		expect(t, actual.At(i), val)
+	})
+}
+
+func TestSort(t *testing.T) {
+	type fixture struct {
+		array    ArrayInterface
+		expected ArrayInterface
+		callback func(a, b interface{}) bool
+	}
+	fixtures := NewArray(&fixture{
+		NewArray(1, 2, 3),
+		NewArray(1, 2, 3),
+		func(a, b interface{}) bool {
+			if a.(int) <= b.(int) {
+				return true
+			}
+			return false
+		},
+	}, &fixture{
+		NewArray(1, 2, 3),
+		NewArray(3, 2, 1),
+		func(a, b interface{}) bool {
+			if a.(int) <= b.(int) {
+				return false
+			}
+			return true
+		},
+	})
+	fixtures.ForEach(func(v interface{}, i int) {
+		fix := v.(*fixture)
+		sorted := fix.array.Sort(fix.callback)
+		fix.expected.ForEach(func(v interface{}, i int) {
+			expect(t, sorted.At(i), v)
+		})
+	})
+
+}
